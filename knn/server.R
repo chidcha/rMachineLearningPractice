84274a7ea7158b573a28f80546c7b4b712285181
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-07-26 13:27:23 +0800

    modified knn to show prediction mechanism

--- knn/server.R
+++ knn/server.R
@@ -8,7 +8,7 @@ server <- function(input, output){
     n <- input$n
     inTrain <- sample(1:nrow(iris), n)
     inTest <- which(1:nrow(iris) %in% inTrain == FALSE)
-    selectInput("variable1", "Choose Option:", as.list(rev(inTest))) 
+    selectInput("variable1", "Choose Observation Number:", as.list(rev(inTest))) 
   })
   
   output$plot1 <- renderPlot({
@@ -19,21 +19,30 @@ server <- function(input, output){
     plot(iris[inTrain,3:4], pch = 1, col = iris[inTrain,5], 
          xlab = "Petal Length", ylab = "Petal Width",
          cex = 1.5, xlim = c(0,7), ylim = c(0,4))
-    testCl <- knn(iris[inTrain,3:4], iris[input$variable1, 3:4], 
-                  iris[inTrain,5], k = nn)
-    
-    points(iris[input$variable1, 3:4], pch = 8, cex = 1.5, col = testCl)
-    
-    
-    
-    myIris <- rbind(iris[input$variable1, 3:4], iris[inTrain,3:4])
-    distM <- as.matrix(dist(myIris))
-    inNn <- as.numeric(names(sort(distM[,1]))[2:(nn+1)])
-    
-    points(iris[inNn,3:4], pch = 3, col = iris[inNn,5])
     legend(x=0, y =4.0, legend = c("Setosa", "Versicolor", "Virginica"),
            pch = 1, col = c("black", "red", "green"), bty = "n",
            cex=1.5)
+    testCl <- knn(iris[inTrain,3:4], iris[input$variable1, 3:4], 
+                  iris[inTrain,5], k = nn)
+    
+    if(input$spoint) {
+      points(iris[input$variable1, 3:4], pch = 8, cex = 1.5)
+    }
+    
+    if(input$nnpoints) {
+      myIris <- rbind(iris[input$variable1, 3:4], iris[inTrain,3:4])
+      distM <- as.matrix(dist(myIris))
+      inNn <- as.numeric(names(sort(distM[,1]))[2:(nn+1)])
+      
+      points(iris[inNn,3:4], pch = 3, col = iris[inNn,5])
+    }
+    
+    if(input$prediction) {
+      points(iris[input$variable1, 3:4], pch = 8, cex = 1.5, col = testCl)
+    }
+    
+    
+    
     
   })
   
--- knn/ui.R
+++ knn/ui.R
@@ -10,8 +10,15 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       numericInput("n", "Number of Observations:", 20, min = 5, max = 150),
+      fluidRow(column(10, h3('Making Prediction'))),
+      uiOutput("testData"),
       numericInput("nn", "No of Nearest Neighbors:", 3, min = 1, max = 17),
-      uiOutput("testData")
+      checkboxInput("spoint", 
+                    "Show the Test Point", value = FALSE),
+      checkboxInput("nnpoints", 
+                    "Show Nearest Neighbours", value = FALSE),
+      checkboxInput("prediction", 
+                    "Make Prediction", value = FALSE)
     ),
     
     mainPanel(
