5ddf053a09409d5925cb3572cc29d758e0f9a4b6
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-07-26 18:23:42 +0800

    included prediction in the SVM model

--- svm/server.R
+++ svm/server.R
@@ -6,18 +6,30 @@ server <- function(input, output){
   myIris <- iris[!inSetosa, c("Petal.Length", "Petal.Width", "Species")]
   myIris$Species <- factor(myIris$Species, levels = c("versicolor", "virginica"))
   
+  set.seed(100)
+  nr <- nrow(myIris)
+  inTrain <- sample(1:nr, 0.6*nr)
+  
+  x1mean <- mean(myIris$Petal.Length[inTrain])
+  x1sd <- sd(myIris$Petal.Length[inTrain])
+  
+  x2mean <- mean(myIris$Petal.Width[inTrain])
+  x2sd <- sd(myIris$Petal.Width[inTrain])
+  
+  output$table <- renderTable({(iris[c(60,110,150),c(3:5)])})
   
   output$plot <- renderPlot({   
-    plot(scale(myIris$Petal.Length), scale(myIris$Petal.Width), pch = 19, col = myIris$Species, 
+    plot(scale(myIris$Petal.Length[inTrain]), scale(myIris$Petal.Width[inTrain]), 
+         pch = 19, col = myIris$Species[inTrain], 
          xlab = "Petal Length", ylab =  "Petal Width", cex.lab = 1.5,
-         cex.axis = 1.5)
+         cex.axis = 1.5, xlim = c(-3,2.8))
     legend(x=3, y = 2.4, legend = c("Versicolor", "Virginica"), 
            col = c("black", "red"), bty = "n", pch = 19, cex = 1.5)
     
     if(input$boundary) {
       if(input$kernal == "linear") {
         svmModel <- svm(Species ~ ., data = myIris, kernal = "linear", 
-                        scale = TRUE, cost = input$cost)
+                        scale = TRUE, cost = input$cost, subset = inTrain)
         w <- t(svmModel$coefs) %*% svmModel$SV
         b <- -svmModel$rho
         
@@ -32,18 +44,27 @@ server <- function(input, output){
         pcol <- ifelse(val$y > 0, "black", "red")
         
         points(val$Petal.Length, val$Petal.Width, col = pcol, cex = 0.02)
+        
+        x1 <- (iris[input$index,3] - x1mean)/x1sd
+        x2 <- (iris[input$index,4] - x2mean)/x2sd
+        
+        if(input$spoint) {
+          points(x1, x2, pch = 8, cex = 2, col = "blue")
+        }
+        
+        if(input$prediction) {
+          yval <- predict(svmModel, data.frame(Petal.Length=iris[input$index,3],
+                                               Petal.Width=iris[input$index,4]))
+          
+          pcol <- ifelse(yval=="versicolor", "black", "red")
+          points(x1, x2, pch = 8, cex = 2, col = pcol)
+        }
       }
       
       if(input$kernal == "radial") {
         
         svmModel <- svm(Species ~ ., data = myIris, kernal = "radial", cost = input$cost,
-                        gamma = input$gamma, scale = TRUE)
-        
-        x1mean <- mean(myIris$Petal.Length)
-        x1sd <- sd(myIris$Petal.Length)
-        
-        x2mean <- mean(myIris$Petal.Width)
-        x2sd <- sd(myIris$Petal.Width)
+                        gamma = input$gamma, scale = TRUE, subset = inTrain)
         
         x1val <- seq(-1,8,length.out = 200)
         x2val <- seq(-1,8, length.out = 200)
@@ -56,16 +77,30 @@ server <- function(input, output){
         xval$Petal.Width  <- (xval$Petal.Width - x2mean)/x2sd
         
         pcol <- ifelse(yval=="versicolor", "black", "red")
-        plot(scale(myIris$Petal.Length), scale(myIris$Petal.Width), pch = 19, 
-             col = myIris$Species,
-             main = "Non-Linear Decision Boundary", 
-             xlab = "scale(Petal.Length)", ylab = "scale(Petal.Width)",
-             xlim = c(-3,2.8))
         points(xval, col = pcol, cex = 0.02)
+        
+        x1 <- (iris[input$index,3] - x1mean)/x1sd
+        x2 <- (iris[input$index,4] - x2mean)/x2sd
+        
+        if(input$spoint) {
+          points(x1, x2, pch = 8, cex = 2, col = "blue")
+        }
+        
+        if(input$prediction) {
+          yval <- predict(svmModel, data.frame(Petal.Length=iris[input$index,3],
+                                               Petal.Width=iris[input$index,4]))
+          
+          pcol <- ifelse(yval=="versicolor", "black", "red")
+          points(x1, x2, pch = 8, cex = 2, col = pcol)
+        }
         
       }
 
     }
+    
+    
+    
+    
     
   })
 }
--- svm/ui.R
+++ svm/ui.R
@@ -13,10 +13,20 @@ ui <- fluidPage(
       numericInput("cost", "Cost", value = 1, min = 0, max = 1000),
       numericInput("gamma", "Gamma", value = 0.5),
       checkboxInput("boundary", 
-                    "Show the decision boundary", value = FALSE)
+                    "Show the decision boundary", value = FALSE),
+      fluidRow(column(10, h3('Making Prediction'))),
+      selectInput("index", "Observation number", 51:150, 
+                   selected = 100),
+      checkboxInput("spoint", 
+                    "Show the Test Point", value = FALSE),
+      checkboxInput("prediction", 
+                    "Make Prediction", value = FALSE)
     ),
     
     mainPanel(
+      fluidRow(column(10, h4('Data Set'), align = "left")),
+      tableOutput("table"),
+      fluidRow(column(10, h4('Corresponding Points and Decision Boundary'), align = "left")),
       plotOutput("plot")
     )
       )   
