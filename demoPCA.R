e157df1c77f5e2a633884d479c4c6ec24338e898
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-07-29 14:24:10 +0800

    added 2d pca example

--- demoPCA.R
+++ demoPCA.R
@@ -1,16 +1,42 @@
-lapply(iris[,1:4], mean)
-apply(iris[,1:4],2, mean)
-apply(iris[,1:4],2, var)
+###########################################################################
+# Useful for finding low dimensional representation of the correlated data
+###########################################################################
+cor(iris[,1:4])
 
-pcI <- prcomp(iris[,1:4], scale = TRUE)
-pcI$center
-pcI$scale
-pcI$rotation
-dim(pcI$x)
-biplot(pcI,scale=0)
-pcI$sdev
-pcV <- pcI$sdev^2
-pve <- pcV/sum(pcV)
+plot(iris$Petal.Length, iris$Petal.Width, col = iris$Species)
+
+names(iris)
+pc1 <- prcomp(iris[,3:4], scale = TRUE)
+plot(pc1$x[,1], pc1$x[,2], col = iris$Species)
+
+pc1$rotation
+
+iris1 <- scale(iris[,3:4])
+iris1[1:5,]%*%pc1$rotation
+pc1$x[1:5,]
+
+## PCA on full data set
+pc2 <- prcomp(iris[,1:4], scale = TRUE)
+pc2$x[1:3,]
+
+plot(pc2$x[,1], col = iris$Species)
+
+par(mfrow=c(2,2))
+for (i in 1:4){
+  plot(iris[,i], col = iris$Species)
+}
+
+par(mfrow=c(1,1))
+plot(pc2$x[,1], col = iris$Species)
+
+iris2 <- scale(iris[,1:4])
+iris2[1:5,]%*%pc2$rotation
+pc2$x[1:5,]
+
+## % variance explained by  principle components
+pc2$sdev
+pc2V <- pcI$sdev^2
+pve <- pc2V/sum(pc2V)
 pve
 plot(pve, xlab = "Principal Component", ylab = "Proportion of 
      variance explained", ylim=c(0,1), type="b")
@@ -18,13 +44,5 @@ plot(pve, xlab = "Principal Component", ylab = "Proportion of
 plot(cumsum(pve), xlab = "Principal Component", ylab = "Cumulative Proportion of 
      variance explained", ylim=c(0,1), type="b")
 
-install.packages("pls")
-library(pls)
-set.seed(1)
 
-pcModel <- pcr(mpg~., data=mtcars, scale = TRUE, validation = "CV")
-summary(pcModel)
-validationplot(pcModel ,val.type="MSEP")
-predV <- predict(pcModel ,mtcars,ncomp =2) 
-mean((predV-mtcars$mpg)^2)
 
