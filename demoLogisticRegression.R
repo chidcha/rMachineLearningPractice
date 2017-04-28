db3ec347f730f9a052fae68b26937f1349e56026
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2017-04-29 07:44:03 +0800

    added probability plot

--- demoLogisticRegression.R
+++ demoLogisticRegression.R
@@ -16,21 +16,17 @@ test <- myIris[-inTrain,]
 testClass <- myIris[-inTrain,3]
 
 # 3. Model training
-glmModel <- glm(Species ~ Petal.Length, data = myIris, family = binomial(link="logit"))
+glmModel <- glm(Species ~ Petal.Length, data = train, family = binomial(link="logit"))
 
 # 3. Making Prediction
-test[1,]
-predVal <- predict(glmModel, newdata = test[1,], type = "response")
-predVal
-testClass[1]
-
 predVal <- predict(glmModel, newdata = test, type = "response")
+plot(test$Petal.Length, predVal, col = testClass)
+abline(h=0.5)
 
 # 4. Checking prediction accuracy
 predClass <- ifelse(predVal > 0.5, "virginica", "versicolor")
 mean(predClass == testClass)
 table(predClass,testClass)
-
 
 # 5. Adding more predictor variable
 glmModel <- glm(Species ~ Petal.Length + Petal.Width, data = myIris, 
